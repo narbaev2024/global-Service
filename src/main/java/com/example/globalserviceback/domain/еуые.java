429bbf7c9b44a1b0fa804382e16385c659a90f31
arsen sultanaliev <arsenliev0@gmail.com>
2026-03-29 18:37:41 +0600

    первый коммит1

--- src/main/java/com/example/globalserviceback/domain/еуые.java
+++ src/main/java/com/example/globalserviceback/domain/еуые.java
@@ -1,4 +1,9 @@
 package com.example.globalserviceback.domain;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class еуые {
+
 }
