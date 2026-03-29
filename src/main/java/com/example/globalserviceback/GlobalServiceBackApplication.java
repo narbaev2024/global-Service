c32b206deb9c8099af58b05173f943c949ca774a
arsen sultanaliev <arsenliev0@gmail.com>
2026-03-29 18:31:19 +0600

    первый коммит

---
+++ src/main/java/com/example/globalserviceback/GlobalServiceBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.globalserviceback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobalServiceBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GlobalServiceBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/globalserviceback/GlobalServiceBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.globalserviceback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlobalServiceBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
